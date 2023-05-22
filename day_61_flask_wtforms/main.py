f6898f61746b53aff2ca3e7c0e56d574d51fb426
Murat Yalçın ALBAŞ <muratalbas@outlook.com>
2023-05-22 21:50:58 +0300

    day 61 challenge done

---
+++ day_61_flask_wtforms/main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+from flask_bootstrap import Bootstrap
+
+
+class LoginForm(FlaskForm):
+    email = EmailField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label='Log In')
+
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    login_form = LoginForm(meta={'csrf': False})
+    if login_form.validate_on_submit():
+        if login_form.email.data == 'admin@email.com' and login_form.password.data == '12345678':
+            return render_template('success.html')
+        return render_template('denied.html')
+    return render_template('login.html', form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
